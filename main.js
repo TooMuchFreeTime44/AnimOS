33cbb69e23f21ef6f668a0c76332d78b6e6101cf
TooMuchFreeTime44 <jrkeliher@gmail.com>
2026-08-12 10:20:46 -0500

    Added some squash and stretch, imperfect

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-function drawArrow(t, f) {
+function drawArrow(t, f, v, h) {
     push();
     translate(t * width / f - 2, height / 3);
     fill(60, 255, 140, t * 160);
     beginShape();
-    vertex(-13, 0);
-    vertex(8, 11);
-    vertex(10, 8);
-    vertex(-6, 0);
-    vertex(10, -8);
-    vertex(8, -11);
+    vertex(-13 * h, 0);
+    vertex(8 * h, 11 * v);
+    vertex(10 * h, 8 * v);
+    vertex(-6 * h, 0);
+    vertex(10 * h, -8 * v);
+    vertex(8 * h, -11 * v);
     endShape();
     pop();
 }
@@ -34,17 +34,27 @@ function draw() {
         if (deltaX < 0) deltaX = 0;
         if (deltaX < width / f) {
             strokeWeight(0);
-            let t = (deltaX / (width / f)) * (deltaX / (width / f));
+            let t = 1 - (1 - (deltaX / (width / f))) ** 2;
             if (t > 1) t = 1;
-            fill(4, 156, 0, t * 255);
+            fill(4, 156, 0, (deltaX / (width / f)) * 255);
             rect(t * width / f - rectW / 2 - 2, height / 3 - 20, rectW, 40, 14);
-            drawArrow(t, f);
+            drawArrow(t, f, 1, 1);
         }
-        if (deltaX >= width / f) {
+        if (deltaX >= width / f && deltaX < width / f + 8) {
+            rectMode(CENTER);
             strokeWeight(0);
             fill(4, 156, 0, 255);
-            rect(width / f - rectW / 2 - 2, height / 3 - 20, rectW, 40, 14);
-            drawArrow(1, f);
+            rect(width / f - 2 + (deltaX - width / f) / 4, height / 3, rectW + (deltaX - width / f) / 2, 40 - (deltaX - width / f) / 2, 14);
+            drawArrow(1, f, 1 - (deltaX - width / f) / 80, 1 + (deltaX - width / f) / (2 * rectW));
+            rectMode(CORNER);
+        }
+        if (deltaX >= width / f + 8) {
+            rectMode(CENTER);
+            strokeWeight(0);
+            fill(4, 156, 0, 255);
+            rect(width / f, height / 3, rectW + 4, 36, 14);
+            drawArrow(1, f, 0.9, 1 + 4 / rectW);
+            rectMode(CORNER);
         }
     }
 }
